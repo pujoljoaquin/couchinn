584b5c72656acef8e9f4e6dbbdab6182d0608ae0
Pau Erbino <paulaerbino@yahoo.com.ar>
2015-11-11 13:27:42 -0300

    Tablas tipocouch hechas

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 puts "Creo los users"
 User.find_or_create_by(nombre:"Cletus", apellido:"Medias", email:"cletus@hotmail.com", nombreUsuario:"cletus", nacionalidad:"Argentina", masculino: true, esPremium:false, foto:"http://vignette3.wikia.nocookie.net/lossimpson/images/6/6b/Cletus001.gif/revision/latest?cb=20110106170451&path-prefix=es" )
 User.find_or_create_by(nombre:"Cristina", apellido:"Kirchner", email:"lacris@hotmail.com", nombreUsuario:"cristina", nacionalidad:"Argentina", masculino: false, esPremium:true, foto:"http://www.ellitoralconcordia.com/wp-content/uploads/cristina-kirchner20.jpg" )
-User.find_or_create_by(nombre:"Susana", apellido:"Gimenez", email:"lasu@hotmail.com", nombreUsuario:"susana", nacionalidad:"Argentina", masculino: false, esPremium:false, foto:"http://www.fundacionkonex.org/img_curriculums/7957b051f25427efbdb3bba77eef11d15be6.jpg")
+User.find_or_create_by(nombre:"Susana", password:"12345678", password_confirmation: "12345678", apellido:"Gimenez", email:"lasu@hotmail.com", nombreUsuario:"susana", nacionalidad:"Argentina", masculino: false, esPremium:false, foto:"http://www.fundacionkonex.org/img_curriculums/7957b051f25427efbdb3bba77eef11d15be6.jpg")
 
 puts "Creo los Couch_types"
 CouchType.find_or_create_by(nombre:"Casa", activo:true)
