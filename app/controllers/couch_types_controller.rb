b964d71d55a0d8c926659d51920beecb6968f0e2
federica <fdelgadodeleon@gmail.com>
2015-11-12 10:35:43 -0300

    boton modificar

--- app/controllers/couch_types_controller.rb
+++ app/controllers/couch_types_controller.rb
@@ -1,5 +1,5 @@
 class CouchTypesController < ApplicationController
-before_action :get_couch_type, only: [:edit, :update, :destroy]
+before_action :get_couch_type, only: [:edit, :update]
 
 def get_couch_type
 	@type = CouchType.find(params[:id])
@@ -15,7 +15,7 @@ def create
 end
 
 def update
-	#tipo.activo=true
+	@type.update(params.require(:couch_type).permit(:nombre,:activo))
 	redirect_to couch_types_path
 end
 
@@ -23,11 +23,6 @@ def index
 end
 
 def edit
-end
-
-def destroy
-	tipo.activo=false
-	redirect_to couch_types_path
 end
 
 end
