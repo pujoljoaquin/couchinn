a279f7cd41fef85d07968107d0482d1e8847c85c
Joaquin Pujol <pujoljoaquin@hotmail.com>
2015-11-26 10:22:30 -0300

    Medalla al lado de nombre de couch de propietario premium

--- app/controllers/premiums_controller.rb
+++ app/controllers/premiums_controller.rb
@@ -3,7 +3,7 @@ class PremiumsController < ApplicationController
 def index
   if params[:commit] == "Pagar"
   	current_user.serPremium
-    redirect_to users_show_path, notice: "Felicitaciones!!! Ya es premium!"
+    redirect_to users_show_path, notice: "Felicitaciones!!! Ya sos premium!"
   end
 end
 
