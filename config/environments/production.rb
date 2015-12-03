ebe739d4ac3237a8ae0fd28f51107f8b4ad49980
Joaquin Pujol <pujoljoaquin@hotmail.com>
2015-12-03 17:51:55 -0300

    Analizando si anda el reset password en heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { :host => 'couchinnsoft.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'couchinnsoft.herokuapp.com'
+  #Rails.application.routes.default_url_options[:host] = 'couchinnsoft.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -10,11 +10,9 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    #address: "smtp.gmail.com",
-    address: 'smtp.sendgrid.net',
+    address: "smtp.gmail.com",
     port: 587,
-    #domain: 'gmail.com',
-    domain: 'heroku.com',
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "joaquin.pujol95@gmail.com",
