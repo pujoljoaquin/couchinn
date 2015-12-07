a26370228fda7935941e5f21f1da11fa90a55067
Joaquin Pujol <pujoljoaquin@hotmail.com>
2015-12-07 11:43:32 -0300

    analizando reset password en heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { :host => 'https://couchinnsoft.herokuapp.com/' }
-  #Rails.application.routes.default_url_options[:host] = 'https://couchinnsoft.herokuapp.com/'
+  Rails.application.routes.default_url_options[:host] = 'https://couchinnsoft.herokuapp.com/'
 
   config.action_mailer.delivery_method = :smtp
   #config.action_mailer.perform_deliveries = true
